4a4c459613a05a544969f58c4be73d213180f9a9
ThiagoMacedo2004 <tsouzamacedo@gmail.com>
2025-09-08 23:44:40 -0300

    Gerando componente cliente e o service no front

---
+++ front-end-cadastro-cliente/src/app/cliente/cliente.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cliente',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cliente.component.html',
+  styleUrls: ['./cliente.component.css']
+})
+export class ClienteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
