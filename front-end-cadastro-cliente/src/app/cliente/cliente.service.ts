1002f70503003a9d93ae84f62bee4a2d08fa44ef
ThiagoMacedo2004 <tsouzamacedo@gmail.com>
2025-09-18 22:34:46 -0300

    add numero do endereço no detalhe do cliente.

--- front-end-cadastro-cliente/src/app/cliente/cliente.service.ts
+++ front-end-cadastro-cliente/src/app/cliente/cliente.service.ts
@@ -7,12 +7,12 @@ import { delay, first } from 'rxjs';
 })
 export class ClienteService {
 
-  private BASE_PATH = 'http://localhost/projeto-faculdade-impacta/back-end-cadastro-cliente/src/controller/ClienteController.php'
-  private API_CEP = 'http://localhost/projeto-faculdade-impacta/back-end-cadastro-cliente/src/api/CepApi.php'
+  // private BASE_PATH = 'http://localhost/projeto-faculdade-impacta/back-end-cadastro-cliente/src/controller/ClienteController.php'
+  // private API_CEP = 'http://localhost/projeto-faculdade-impacta/back-end-cadastro-cliente/src/api/CepApi.php'
 
   // endpoint notebook
-  // private API_CEP = 'http://localhost/projeto-faculdade/projeto-faculdade-impacta/back-end-cadastro-cliente/src/api/CepApi.php'
-  // private BASE_PATH = 'http://localhost/projeto-faculdade/projeto-faculdade-impacta/back-end-cadastro-cliente/src/controller/ClienteController.php'
+  private API_CEP = 'http://localhost/projeto-faculdade/projeto-faculdade-impacta/back-end-cadastro-cliente/src/api/CepApi.php'
+  private BASE_PATH = 'http://localhost/projeto-faculdade/projeto-faculdade-impacta/back-end-cadastro-cliente/src/controller/ClienteController.php'
 
 
 
@@ -21,7 +21,7 @@ export class ClienteService {
   ) { }
 
   public gravarNovoCliente(obj: any) {
-    return this._http.post(this.BASE_PATH, obj).pipe(first(), delay(4000))
+    return this._http.post(this.BASE_PATH, obj).pipe(first(), delay(0))
   }
 
   public getClientes() {
@@ -29,7 +29,7 @@ export class ClienteService {
       params: {
         acao: 'getClientes'
       }
-    }).pipe(first(), delay(3000))
+    }).pipe(first(), delay(0))
   }
 
   public apiCep(cep: any) {
